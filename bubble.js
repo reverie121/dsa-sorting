d7487e87e31a3641b23c457797d63472d222accc
Jeremy Sobczak <code@jerspace.com>
2023-08-10 12:03:21 -0400

    Added bubbleSort (Quadratic Sorting) function with optimization.

--- bubble.js
+++ bubble.js
@@ -1,3 +1,19 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    let i = 0;
+    for (i; i < arr.length; i++) {
+        let swapped = false;
+        let j = 0;
+        for (j; j < arr.length; j++) {
+            if (arr[j] > arr[j+1]) {
+                let holder = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = holder;
+                swapped = true;
+            }
+        }
+        if (swapped === false) break;
+    }
+    return arr;
+}
 
 module.exports = bubbleSort;
