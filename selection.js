b76e28cc1cdc9b852320e8c24d6b7c08fd94eed1
Jeremy Sobczak <code@jerspace.com>
2023-08-10 12:55:11 -0400

    Added selectionSort (Quadratic Sorting) function.

--- selection.js
+++ selection.js
@@ -1,3 +1,18 @@
-function selectionSort() {}
+function selectionSort(arr) {        
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[j] < arr[min]) {
+                min=j; 
+            }
+         }
+         if (min != i) {
+             let holder = arr[i]; 
+             arr[i] = arr[min];
+             arr[min] = holder;      
+        }
+    }
+    return arr;
+}
 
 module.exports = selectionSort;
